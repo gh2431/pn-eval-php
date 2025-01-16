bbded9595c560389b91ac1876944664f8f2e7e77
gh2431 <helygael16@gmail.com>
2025-01-16 11:49:55 +0100

    début

--- view/homeView.php
+++ view/homeView.php
@@ -9,6 +9,9 @@
     <script src="../script/bootstrap.bundle.js"></script>
 </head>
 <body>
+<div class="form-line">
+     <input type="submit" value="Se connecter" class="login-bouton">
+</div>       
     <!-- Créer un formulaire pour récupérer le nom de l'utilisateur connecté -->
 </body>
 </html>
